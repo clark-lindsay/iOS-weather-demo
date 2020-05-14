135a8f267b3df7292392bebd4c03cde1c4913b5e
clark-lindsay <clarklindsay96@gmail.com>
2020-05-14 12:51:30 -0400

    Pulling temperature and name from the API, and displaying it correctly to the screen

--- Weather-Demo/ViewController.swift
+++ Weather-Demo/ViewController.swift
@@ -9,9 +9,29 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var locationLabel: UILabel!
+    @IBOutlet weak var temperatureLabel: UILabel!
     override func viewDidLoad() {
         super.viewDidLoad()
+        let url = URL(string: "http://localhost:3000/weather/location/Arvada,CO")
+        let session = URLSession.shared
+        let task = session.dataTask(
+            with: url!, completionHandler: {(data, response, error) in
+                if (data != nil) {
+                    let json: NSDictionary?
+                    do { json = try
+                        JSONSerialization.jsonObject( with: data!, options: JSONSerialization.ReadingOptions()
+                        ) as? NSDictionary
+                        DispatchQueue.main.async {
+                            let currentTemp: Int = json!.object(forKey: "currentTemperature") as! Int
+                            let locationName: String = json!.object(forKey: "name") as! String
+                            self.temperatureLabel.text = "\(currentTemp)ºF"
+                            self.locationLabel.text = locationName
+                        }
+                    } catch let e {
+                        print("Error retrieving weather data: \(e)") }
+                } })
+        task.resume()
         // Do any additional setup after loading the view.
     }
 
