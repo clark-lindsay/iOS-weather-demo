89554287b154c7b0b6c64a1a0a5d7a7173fde80a
clark-lindsay <clarklindsay96@gmail.com>
2020-05-14 12:25:49 -0400

    Initial Commit

---
+++ Weather-Demo/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Weather-Demo
+//
+//  Created by Clark Lindsay on 5/14/20.
+//  Copyright © 2020 Clark Lindsay. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
